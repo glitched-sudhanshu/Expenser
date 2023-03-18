8dab994db8394b8a3f9940ab88267205b628b05e
sudhanshu <r02.sudhanshu@gmail.com>
2023-03-18 09:12:33 +0530

    Personal expense app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Text('CHART!'),
+          ),
+          Card(
+            child: Text('List of Transactions!'),
+          )
+        ],
+      ),
+    );
+  }
+}
